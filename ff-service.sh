7371db486537aa43ab2a8265c7f2c1601216ecc6
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2022-04-29 18:28:37 +0200

    also support waterfox

--- ff-service.sh
+++ ff-service.sh
@@ -18,5 +18,19 @@ if test -f "$SETTINGS/user.js"; then
             echo $d/user.js symlinked to $SETTINGS/user.js
         fi
     done
+    echo Now set config for waterfox ...
+    for d in $HOME/.waterfox/*-default/ ; do
+        if test -f "$d/user.js"; then
+            if [ "$(readlink -- "$d/user.js")" = $SETTINGS/user.js ]; then
+                echo $d/user.js already symlinked to $SETTINGS/user.js
+            else
+                echo $d/user.js exists with value, not changing
+            fi
+        else
+            cd $d
+            ln -s $SETTINGS/user.js
+            echo $d/user.js symlinked to $SETTINGS/user.js
+        fi
+    done    
 fi
 
